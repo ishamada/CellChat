cdccda2fcaa9de4695fafb3696731d0dd58e3b9b
Islam Hamada <islamsalah@gmail.com>
2023-02-16 00:22:23 +0200

    tutorial first commit

---
+++ ccTut.R
@@ -0,0 +1,68 @@
+
+library(CellChat)
+library(patchwork)
+options(stringsAsFactors = FALSE)
+
+
+# Here we load a scRNA-seq data matrix and its associated cell meta data
+load(url("https://ndownloader.figshare.com/files/25950872")) # This is a combined data from two biological conditions: normal and diseases
+#load("/Users/jinsuoqin/Documents/CellChat/tutorial/data_humanSkin_CellChat.rda")
+data.input = data_humanSkin$data # normalized data matrix
+meta = data_humanSkin$meta # a dataframe with rownames containing cell mata data
+cell.use = rownames(meta)[meta$condition == "LS"] # extract the cell names from disease data
+
+# Prepare input data for CelChat analysis
+data.input = data.input[, cell.use]
+meta = meta[cell.use, ]
+# meta = data.frame(labels = meta$labels[cell.use], row.names = colnames(data.input)) # manually create a dataframe consisting of the cell labels
+unique(meta$labels) # check the cell labels
+
+unique(meta$patient.id)
+
+# Create a CellChat object
+cellchat <- createCellChat(object = data.input, meta = meta, group.by = "labels")
+
+
+cellchat <- addMeta(cellchat, meta = meta)
+cellchat <- setIdent(cellchat, ident.use = "labels") # set "labels" as default cell identity
+levels(cellchat@idents) # show factor levels of the cell labels
+groupSize <- as.numeric(table(cellchat@idents)) # number of cells in each cell group
+
+CellChatDB <- CellChatDB.human # use CellChatDB.mouse if running on mouse data
+showDatabaseCategory(CellChatDB)
+
+# Show the structure of the database
+dplyr::glimpse(CellChatDB$interaction)
+
+# use a subset of CellChatDB for cell-cell communication analysis
+CellChatDB.use <- subsetDB(CellChatDB, search = "Secreted Signaling") # use Secreted Signaling
+# use all CellChatDB for cell-cell communication analysis
+# CellChatDB.use <- CellChatDB # simply use the default CellChatDB
+
+# set the used database in the object
+cellchat@DB <- CellChatDB.use
+
+# subset the expression data of signaling genes for saving computation cost
+cellchat <- subsetData(cellchat) # This step is necessary even if using the whole database
+
+library(doParallel)
+detectCores()
+registerDoParallel(cores = 4)
+
+future::plan("multiprocess", workers = 4) # do parallel
+
+cellchat <- identifyOverExpressedGenes(cellchat)
+cellchat <- identifyOverExpressedInteractions(cellchat)
+
+# project gene expression data onto PPI (Optional: when running it, USER should set `raw.use = FALSE` in the function `computeCommunProb()` in order to use the projected data)
+cellchat <- projectData(cellchat, PPI.human)
+
+
+
+
+
+
+
+
+
+
